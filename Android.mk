b6a515624e2f7e1a1592c2f2ab83243486022900
JezSonic <karoltomaszewskimusic@gmail.com>
2021-03-12 11:38:25 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,34 +2,34 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE:= avcodec
-LOCAL_SRC_FILES := $(LOCAL_PATH)/prebuilts/$(TARGET_ARCH_ABI)/libavcodec.so
+LOCAL_SRC_FILES := prebuilts/$(TARGET_ARCH_ABI)/libavcodec.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 LOCAL_MODULE:= avdevice
-LOCAL_SRC_FILES := $(LOCAL_PATH)/prebuilts/$(TARGET_ARCH_ABI)/libavdevice.so
+LOCAL_SRC_FILES := prebuilts/$(TARGET_ARCH_ABI)/libavdevice.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 LOCAL_MODULE:= avfilter
-LOCAL_SRC_FILES := $(LOCAL_PATH)/prebuilts/$(TARGET_ARCH_ABI)/libavfilter.so
+LOCAL_SRC_FILES := prebuilts/$(TARGET_ARCH_ABI)/libavfilter.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 LOCAL_MODULE:= avformat
-LOCAL_SRC_FILES := $(LOCAL_PATH)/prebuilts/$(TARGET_ARCH_ABI)/libavformat.so
+LOCAL_SRC_FILES := prebuilts/$(TARGET_ARCH_ABI)/libavformat.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 LOCAL_MODULE:= swresample
-LOCAL_SRC_FILES := $(LOCAL_PATH)/prebuilts/$(TARGET_ARCH_ABI)/libswresample.so
+LOCAL_SRC_FILES := prebuilts/$(TARGET_ARCH_ABI)/libswresample.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 LOCAL_MODULE:= swscale
-LOCAL_SRC_FILES := $(LOCAL_PATH)/prebuilts/$(TARGET_ARCH_ABI)/libswscale.so
+LOCAL_SRC_FILES := prebuilts/$(TARGET_ARCH_ABI)/libswscale.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 LOCAL_MODULE:= avutil
-LOCAL_SRC_FILES := $(LOCAL_PATH)/prebuilts/$(TARGET_ARCH_ABI)/libavutil.so
+LOCAL_SRC_FILES := prebuilts/$(TARGET_ARCH_ABI)/libavutil.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 LOCAL_MODULE := "godot-videodecoder"
 LOCAL_SHARED_LIBRARIES := avcodec avdevice avfilter avformat swresample swscale avutil
-LOCAL_SRC_FILES := $(LOCAL_PATH)/src/gdnative_videodecoder.c
+LOCAL_SRC_FILES := src/gdnative_videodecoder.c
 include $(BUILD_SHARED_LIBRARY)
